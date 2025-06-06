d210b8130efb75a059c61908e71f3e88e802152e
Songa7273 <k.gaju@alustudent.com>
2025-06-06 10:43:31 +0200

    rain project

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-"""create an algorithm for calculating the rain"""
+"""
+rain module
+This module contains the rain function that computes the amount of water
+retained after raining on a relief map represented as a list of integers.
+"""
 
 
 def rain(walls):
-    """calculate how much water will be retained after it rains"""
+    """
+    Calculate how many square units of water will be retained after it rains.
+
+    Args:
+        walls (list): A list of non-negative integers representing wall heights.
+
+    Returns:
+        int: Total amount of water retained.
+    """
     if not walls or len(walls) < 3:
         return 0
+
+    left = 0
+    right = len(walls) - 1
+    left_max = walls[left]
+    right_max = walls[right]
     water = 0
-    for i in range(1, len(walls) - 1):
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
-        right = walls[i]
-        for j in range(i + 1, len(walls)):
-            right = max(right, walls[j])
-        water += min(left, right) - walls[i]
+
+    while left < right:
+        if walls[left] < walls[right]:
+            left += 1
+            left_max = max(left_max, walls[left])
+            water += max(0, left_max - walls[left])
+        else:
+            right -= 1
+            right_max = max(right_max, walls[right])
+            water += max(0, right_max - walls[right])
+
     return water
